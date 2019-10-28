d25a4260e582054604c382011eec47e3cd87054c
Mark Chesney <markplotlib@gmail.com>
2019-10-27 17:18:13 -0700

    remove prototype function

--- driver/Driver.java
+++ driver/Driver.java
@@ -35,25 +35,7 @@ public class Driver {
         printSystem(system);
     }
 
-    private static void tempPrototype(RegistrationSystem system) throws DuplicatePersonException, DuplicateSubjectException {
-        // system.addFaculty("Roshanak", "Roshandel", FacultyType.ASSOCPROF, Building.ENGR, 508, "roshanak@seattleu.edu");
-        system.addFaculty("Roshanak", "Roshandel", FacultyType.ASSOCPROF, Building.ENGR, 508, "obarej@seattleu.edu");
-        system.addFaculty("James", "Obare", FacultyType.INSTRUCT, Building.LEML, 122, "roshanak@seattleu.edu");
-
-        system.addStudent("Alan", "Turing", StudentType.GRAD, StudentProgram.CERT, Quarter.RQ, 2017);
-        system.addStudent("Peter", "Gabriel", StudentType.UNDERGRAD, StudentProgram.CERT, Quarter.RQ, 2019);
-        system.addStudent("James", "Brown", StudentType.UNDERGRAD, StudentProgram.MSCS, Quarter.RQ, 2018);
-    }
-
     private static void populateSystem(RegistrationSystem system) {
-        try {
-			tempPrototype(system);
-		} catch (DuplicatePersonException | DuplicateSubjectException e1) {
-            System.out.println(e1.getMessage());
-			e1.printStackTrace();
-		}
-
-
         try {
             populateFaculty(system);
             populateStudents(system);
